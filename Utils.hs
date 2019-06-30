cd63efcfc864b6a0161be6d20d4a2fa919021c74
LuizBoina <lipeboina.boina@hotmail.com>
2019-06-30 17:49:32 -0300

    adicionando melhorTeste

--- Utils.hs
+++ Utils.hs
@@ -2,8 +2,9 @@ module Utils(
 formataCaracteristicas,
 formataExemplos,
 listaCaracteristicas,
---temMesmaClassificacao,
--- criaArvoreDecisao,
+discretizaNumeral,
+maioria,
+criaArvoreDecisao,
 -- formataArvore,
 -- computaResultado
 ) where
@@ -33,17 +34,17 @@ formataCaracteristicas :: [[String]] -> Caracteristicas
 formataCaracteristicas [] = []
 formataCaracteristicas (xs:xss)
                     | length xs == 1 = (Numeral (head xs) []):(formataCaracteristicas xss)
-                    | otherwise = (Nominal (head xs) (tail xs)):(formataCaracteristicas xss)
+                    | otherwise = (Nominal (head xs) (sort $ tail xs)):(formataCaracteristicas xss)
 
-{- criaArvoreDecisao [] _ maisComum = maisComum
+criaArvoreDecisao [] _ maisComum = maisComum
 criaArvoreDecisao exemplos [] _ = maioria exemplos
 criaArvoreDecisao exemplos caracteristicas maisComum
-                        | temMesmaClassificacao exemplos = classificacaoEx (head exemplos)
-                        | otherwise = do let melhor = melhorTeste caracteristicas exemplos
-                                         let arvore = criaRaiz melhor
- -}
+                        | temMesmaClassificacao exemplos = pegaClassiEx (head exemplos)
+                        | otherwise = do let arvore = criaRaiz (melhorTeste exemplos caracteristicas (-999) (head caracteristicas))
+                                         putStrLn $ show arvore
 
-classificacaoEx (T caract classificacao) = classificacao
+readFloat :: String -> Double
+readFloat = read
 
 temMesmaClassificacao :: Exemplos -> Bool
 temMesmaClassificacao ((T caracteristicas classificacao):exemplos) = and [classificacao == _classificacao | (T _caracteristicas _classificacao) <- exemplos]
@@ -57,8 +58,9 @@ dividirPorClasseExemplos [] = []
 dividirPorClasseExemplos exemplos = (takeWhile (==(head exemplos)) exemplos) ++  dropWhile (==(head exemplos)) exemplos
 
 percentagemClasse classes = [(fromIntegral $ length mesmaClasse)/n | mesmaClasse <- classes]
-                            where n = fromIntegral $ product $ map length classes
+                            where n = fromIntegral $ sum $ map length classes
 
+maioria :: Exemplos -> String
 maioria exemplos = classeMajoritaria (ordenaClasseEx exemplos) []
                    where classeMajoritaria [] majoritaria = head majoritaria
                          classeMajoritaria classes majoritaria
@@ -67,29 +69,74 @@ maioria exemplos = classeMajoritaria (ordenaClasseEx exemplos) []
                            where classeAtual = takeWhile (==(head classes)) classes
                                  restante = dropWhile (==(head classes)) classes
 
--- melhorTeste caracteristicas exemplos = do let ig = calculaIG exemplos caracteristicas
-
-pegaValores (Numeral caract valores) = sort valores
-pegaValores (Nominal caract valores) = sort valores
+pegaClassiEx (T caract classificacao) = classificacao
 pegaNomeCaract (Numeral caract valores) = caract
 pegaNomeCaract (Nominal caract valores) = caract
 
-calculaEntropia percentages = sum [-percentagem*(log percentagem) | percentagem <- percentages]
+calculaEntropia :: [Double] -> Double
+calculaEntropia [] = 0
+calculaEntropia (percentagem:percentages)
+                        | percentagem == 0 = calculaEntropia percentages
+                        | otherwise = -percentagem*(logBase 2 percentagem) + calculaEntropia percentages
 
-{- calculaIGR exemplos caracteristica = entropiaExemplos - (somatorioIG (pegaClassiCaract exemplos (pegaNomeCaract caracteristica)) valores)/(fromIntegral $ length exemplos)
-                                     where entropiaExemplos = calculaEntropia (percentagemClasse (dividirPorClasseExemplos (ordenaClasseEx exemplos)))
-                                           valores = calculaFreq (pegaValoresEx exemplos (pegaNomeCaract caracteristica)) (pegaValores caracteristica) -}
+melhorTeste :: Exemplos -> Caracteristicas -> Double -> Caracteristica -> Caracteristica
+melhorTeste _ [] _ caractMaiorIGR = caractMaiorIGR
+melhorTeste exemplos (caracteristica:caracteristicas) maiorIGR caractMaiorIGR
+                                                      | maiorIGR < atualIGR = melhorTeste exemplos caracteristicas atualIGR caracteristica
+                                                      | otherwise = melhorTeste exemplos caracteristicas maiorIGR caractMaiorIGR
+                                                      where atualIGR
+                                                                  | somaIV == 0 = 0
+                                                                  | otherwise = (entropiaExemplos - somaIG)/somaIV
+                                                            entropiaExemplos = calculaEntropia (percentagemClasse (dividirPorClasseExemplos (ordenaClasseEx exemplos)))
+                                                            somaIG = ((somatorioIG (pegaClassiCaract exemplos (pegaNomeCaract caracteristica)) valorFreq)/(fromIntegral $ length exemplos))
+                                                            valorFreq = calculaFreq (pegaValoresEx exemplos (pegaNomeCaract caracteristica)) caracteristica
+                                                            somaIV = -((somatorioIV (pegaClassiCaract exemplos (pegaNomeCaract caracteristica)) valorFreq)/(fromIntegral $ length exemplos))
+
+somatorioIV :: [(String, String)] -> [(String, Int)] -> Double
+somatorioIV _ [] = 0
+somatorioIV exemplos (valorFreq:valores) 
+                    | snd valorFreq == 0 = somatorioIV exemplos valores
+                    | otherwise = (fromIntegral $ snd valorFreq)*(logBase 2 ((fromIntegral $ snd valorFreq)/(fromIntegral $ length exemplos))) + (somatorioIV exemplos valores)
+
+--entrada: lista de tupla contendo (valor, classe) e uma lista de tupla contendo (valor, frequencia)
+--saida: somatorio IG de todos valores da dada caracteristica
+somatorioIG :: [(String, String)] -> [(String, Int)] -> Double
 somatorioIG _ [] = 0
-somatorioIG exemplos (valor:valores) = (snd valor)*(calculaEntropia (percentagemClasse (pegaClassiValor exemplos (fst valor))))/(fromIntegral $ length exemplos) + (somatorioIG exemplos valores)
+somatorioIG exemplos (valorFreq:valores) = (fromIntegral $ snd valorFreq)*(calculaEntropia (percentagemClasse (pegaClassiValor exemplos (fst valorFreq)))) + (somatorioIG exemplos valores)
 
+--entrada: uma lista de tupla de (valor, classe) da base e um valor
+--saida: lista de todas as classificacoes para o valor na base ordenado e semparado em sublistas
+pegaClassiValor :: [(String, String)] -> String -> [String]
 pegaClassiValor exemplos valor = dividirPorClasseExemplos $ sort [snd exemplo | exemplo <- exemplos, (fst exemplo) == valor]
 
+--entrada: lista de exemplos e nome de uma caracteristica
+--saida: valores ordenados dessa caracteristica na base de exemplos
+pegaValoresEx :: Exemplos -> String -> [String]
 pegaValoresEx exemplos caract = sort [snd _caract | (T _caracts classi) <- exemplos, _caract <- _caracts, (fst _caract) == caract]
 
+--entrada: lista de exemplos e uma dada caracteristica
+--saida: lista de tupla contendo o valor da caracteristica e a classificacao que o exemplo recebeu
+pegaClassiCaract :: Exemplos -> String -> [(String, String)]
 pegaClassiCaract exemplos caracteristica = [(snd _caractEx, classi) | (T caractEx classi) <-exemplos, _caractEx <- caractEx, (fst _caractEx) == caracteristica]
 
---considerando que ambos estao ordenados e que tem a mesma ordem
-calculaFreq _ [] = []
-calculaFreq valoresEx (valor:valores)
-                      | length valoresEx == 0 || head valoresEx /= valor = (valor, 0):(calculaFreq valoresEx valores)
-                      | otherwise = (valor ,(length (takeWhile (== valor) valoresEx))):(calculaFreq (dropWhile (== valor) valoresEx) valores)
+--entrada: valores ordenados da base em relacao a uma dada caracteristica e caracteristica
+--saida: lista de tupla contendo o valor da caracteristica e a frequencia que aparece na base de exemplos
+calculaFreq :: [String] -> Caracteristica -> [(String, Int)]
+calculaFreq _ (Nominal caract []) = []
+calculaFreq valoresEx (Nominal caract (valor:valores))
+                      | length valoresEx == 0 || head valoresEx /= valor = (valor, 0):(calculaFreq valoresEx (Nominal caract valores))
+                      | otherwise = (valor ,(length (takeWhile (== valor) valoresEx))):(calculaFreq (dropWhile (== valor) valoresEx) (Nominal caract valores))
+calculaFreq _ (Numeral caract []) = []
+calculaFreq valoresEx (Numeral caract (valor:valores))
+                      | length valoresEx == 0 || head valoresEx > valor = (valor, 0):(calculaFreq valoresEx (Numeral caract valores))
+                      | otherwise = (valor ,(length (takeWhile (<= valor) valoresEx))):(calculaFreq (dropWhile (<= valor) valoresEx) (Numeral caract valores))
+          
+discretizaNumeral :: Caracteristicas -> Exemplos -> Caracteristicas
+discretizaNumeral [] _ = []
+discretizaNumeral ((Nominal caract valores):caracts) exemplos = (Nominal caract valores):(discretizaNumeral caracts exemplos)
+discretizaNumeral (((Numeral caract [])):caracts) exemplos = (Numeral caract (discretizaValor++["9999"])):(discretizaNumeral caracts exemplos)
+                                                             where discretizaValor = calculaMedias (sort (pegaClassiCaract exemplos caract))
+                                                                   calculaMedias [ex] = []
+                                                                   calculaMedias (ex:exs)
+                                                                                   | snd ex == snd (head exs) = calculaMedias exs
+                                                                                   | otherwise = (show (((readFloat (fst ex))+(readFloat (fst (head exs))))/2.0)):(calculaMedias exs)
--- main.hs
+++ main.hs
@@ -1,13 +1,14 @@
 import Io
 import Utils
 
-main = do _descricao <- lerDescricao
-          let descricao = formataCaracteristicas _descricao --init descricao = caracteristicas/ last descricao = classe
+main = do descricao <- lerDescricao
+          let _descricao = formataCaracteristicas descricao --init descricao = caracteristicas/ last descricao = classe
           base <- lerBase
-          let exemplos = formataExemplos base (listaCaracteristicas (init descricao))
-          putStrLn (show exemplos)
-          {- let arvoreDecisao = criaArvoreDecisao (formataExemplos base) (formataCaracteriscas descricao)
-          imprimeArvore (formataArvore arvoreDecisao)
+          let exemplos = formataExemplos base (listaCaracteristicas (init _descricao))
+          let __descricao = discretizaNumeral (init _descricao) exemplos
+          criaArvoreDecisao exemplos __descricao (maioria exemplos)   -- let arvoreDecisao = criaArvoreDecisao exemplos __descricao (maioria exemplos)
+          {-imprimeArvore (formataArvore arvoreDecisao)
           caso <- lerCaso
           let resultado = computaResultado arvoreDecisao caso
           imprimeResult resultado -}
+        --   melhorTeste [T [("Aparencia","Sol"),("Temperatura","25"),("Umidade","72"),("Vento","Sim")] "Va",T [("Aparencia","Sol"),("Temperatura","28"),("Umidade","91"),("Vento","Sim")] "NaoVa",T [("Aparencia","Sol"),("Temperatura","22"),("Umidade","70"),("Vento","Nao")] "Va",T [("Aparencia","Sol"),("Temperatura","23"),("Umidade","95"),("Vento","Nao")] "NaoVa",T [("Aparencia","Sol"),("Temperatura","30"),("Umidade","85"),("Vento","Nao")] "NaoVa"] [Nominal "Aparencia" ["Sol","Chuva","Nublado"],Numeral "Temperatura" ["22.5","24.0","26.5"],Numeral "Umidade" ["78.5"],Nominal "Vento" ["Sim","Nao"]] (-9999) (Nominal "Vento" ["Sim","Nao"])
